8b1d7ba0942bb60ca20314a172736ec935aad938
Minecraft5310 <62607154+Minecraft5310@users.noreply.github.com>
2021-07-15 20:08:14 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+  sea = createSprite(200,200,200,200)
+  sea.addImage("sea", seaImg);
+  sea.scale = 0.4;
+  sea.velocityX = -5;
+  ship1 = createSprite(130,250,10,10)
+  ship1.addAnimation("ship",shipImg1)
+  ship1.scale = 0.3;
+}
+
+function draw() {
+  background("blue");
+  if(sea.x < 0){
+    sea.x = 200;
+  }
+ 
+
+drawSprites();
+}
